d29ea2a38eb1665533ca6eea18426e92b88f6d1a
tapioca24 <tapioca_niconico@yahoo.co.jp>
2019-12-01 22:36:45 +0900

    チャンネル選択コンポーネントを追加

---
+++ src/components/CsChunnelSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cs-chunnel-select">
+    <input
+      type="radio"
+      name="chunnel"
+      id="channel-red"
+      value="red"
+      v-model="chunnel"
+    />
+    <label for="chunnel-red">red</label>
+    <input
+      type="radio"
+      name="chunnel"
+      id="channel-green"
+      value="green"
+      v-model="chunnel"
+    />
+    <label for="chunnel-green">green</label>
+    <input
+      type="radio"
+      name="chunnel"
+      id="channel-blue"
+      value="blue"
+      v-model="chunnel"
+    />
+    <label for="chunnel-blue">blue</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+export default class CsChunnelSelect extends Vue {
+  chunnel = "red";
+}
+</script>
